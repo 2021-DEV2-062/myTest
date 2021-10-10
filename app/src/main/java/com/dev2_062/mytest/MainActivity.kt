af9e9ffcbcab6450f360e0c62061c5e521d5fada
Douay Nicolas <douay@saficard.com>
2021-10-10 21:20:03 +0200

    init

---
+++ app/src/main/java/com/dev2_062/mytest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dev2_062.mytest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev2_062.mytest.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
