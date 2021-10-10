fd13ea85c2a218429c2cb9b3519746caa8802e2a
Douay Nicolas <douay@saficard.com>
2021-10-10 22:27:24 +0200

    Enable DataBinding

--- app/src/main/java/com/dev2_062/mytest/ui/main/MainFragment.kt
+++ app/src/main/java/com/dev2_062/mytest/ui/main/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.dev2_062.mytest.R
+import com.dev2_062.mytest.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -16,15 +18,21 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+
+        val fragmentInitializationBinding = DataBindingUtil.inflate<MainFragmentBinding>(
+                inflater, R.layout.main_fragment, container, false
+        )
+        fragmentInitializationBinding.viewModel = viewModel
+        return fragmentInitializationBinding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
