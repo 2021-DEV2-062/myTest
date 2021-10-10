987389be0218cb47dba1b94b3576f0c8133e3b25
Douay Nicolas <douay@saficard.com>
2021-10-10 22:46:00 +0200

    Create onCellClicked method in MainViewModel

--- app/src/main/java/com/dev2_062/mytest/enums/State.kt
+++ app/src/main/java/com/dev2_062/mytest/enums/State.kt
@@ -2,4 +2,12 @@ package com.dev2_062.mytest.enums
 
 enum class State {
     CIRCLE, CROSS, UNDEFINED;
+
+    fun next(): State {
+        return if (this == CIRCLE) {
+            CROSS
+        } else {
+            CIRCLE
+        }
+    }
 }
--- app/src/main/java/com/dev2_062/mytest/ui/main/MainViewModel.kt
+++ app/src/main/java/com/dev2_062/mytest/ui/main/MainViewModel.kt
@@ -1,7 +1,46 @@
 package com.dev2_062.mytest.ui.main
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import com.dev2_062.mytest.GridValidator
+import com.dev2_062.mytest.Referee
+import com.dev2_062.mytest.enums.GridResult
+import com.dev2_062.mytest.enums.State
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private var grid: MutableList<State> = MutableList(9) { State.UNDEFINED }
+
+    private val referee = Referee(GridValidator())
+
+    val currentPlayer = ObservableField(State.CIRCLE)
+
+    val result = ObservableField("");
+
+    init {
+        currentPlayer.set(State.CIRCLE)
+
+        grid = MutableList(9) { State.UNDEFINED }
+    }
+
+    fun onCellClicked(pos: Int) {
+
+        if (grid[pos] != State.UNDEFINED) return
+
+        currentPlayer.get()?.let {
+            grid[pos] = it
+        } ?: return
+
+        when (referee.check(grid)) {
+            GridResult.VICTORY -> {
+                result.set("${currentPlayer.get()?.name} WIN !!!")
+            }
+            GridResult.IN_PROGRESS -> {
+                currentPlayer.set(currentPlayer.get()?.next())
+            }
+            else -> {
+                result.set("DRAW !!!")
+            }
+        }
+    }
 }
